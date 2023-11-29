01ac5741b486f0e385884543bd04cfd17e30c0e1
220003 <220003pb@yse-c.net>
2023-11-29 11:37:26 +0900

    ts

---
+++ ts_basic/hello.js
@@ -0,0 +1,16 @@
+var message = "hello";
+console.log(message);
+//型推論(type Inference)
+var id = 1;
+var email = "test@test.com";
+var isActive = true;
+var none;
+//型定義(type definition)
+// var id:number=1;
+// var email:string="test@test.com";
+// var isActive:boolean=true;
+// var none:any;
+console.log(id, typeof id);
+console.log(email, typeof email);
+console.log(isActive, typeof isActive);
+console.log(none, typeof none);
